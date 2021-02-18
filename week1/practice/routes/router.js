d75fa78ea0b06c7a4ba862b0cf1c3b277ff28e68
toddpolak <todd.polak@bryanuniversity.edu>
2021-02-17 23:46:06 -0800

    Week 1

--- week1/practice/routes/router.js
+++ week1/practice/routes/router.js
@@ -1,8 +1,27 @@
 const express = require('express')
 const router = express.Router()
-const Movie = require('../models.movie')
+const Movie = require('../models/movie')
+
 
 router.route('/')
     .get((req, res, next) => {
-
+        Movie.find((err, movies) => {
+            if (err) {
+                res.status(500)
+                return next(err)
+            }
+            return res.status(200).send(movies)
+        })
     })
+    .post((req, res, next) => {
+        const newMovie = new Movie(req.body)
+        newMovie.save((err, savedMovie) => {
+            if (err) {
+                res.status(500)
+                return next(err)
+            }
+            return res.status(201).send(savedMovie)
+        })
+    })
+
+module.exports = router
--- week1/practice/server.js
+++ week1/practice/server.js
@@ -7,25 +7,17 @@ const mongoose = require('mongoose')
 app.use('/', express.json())
 app.use(morgan('dev'))
 
-
 /*
-
 const MongoClient = require('mongodb').MongoClient
-
 const uri = "mongodb+srv://toddpolak:elliot@cluster0.0pymy.mongodb.net/sample_airbnb?retryWrites=true&w=majority"
-
 const client = new MongoClient(uri, { useNewUrlParser: true })
-
 console.log(client)
-
 client.connect(err => {
     //const collection = client.db("test").collection("devices")
     // perform actions on the collection object
     //client.close();
 })
-
 */
-
 
 // Connect to DB
 mongoose.connect('mongodb://localhost:27017/moviedb',
@@ -34,12 +26,19 @@ mongoose.connect('mongodb://localhost:27017/moviedb',
         useUnifiedTopology: true,
         useCreateIndex: true,
         useFindAndModify: false
-
     }
     //() => console.log('Connected to the DB')
 )
     .then(() => console.log("Connected to MongoDB"))
 
+// Routes
+app.use('/movies', require('./routes/router'))
+
+// Error handler
+app.use((err, req, res, next) => {
+    console.log(err)
+    return res.send({ errMsg: err.message })
+})
 
 // Server Listen
 app.listen(9000, () => {
