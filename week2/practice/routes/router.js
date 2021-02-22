750d9f61260dd8ff3add9959856a6c17d723e08b
toddpolak <todd.polak@bryanuniversity.edu>
2021-02-21 22:37:57 -0800

    Week 2

---
+++ week2/practice/routes/router.js
@@ -0,0 +1,50 @@
+const express = require('express')
+const router = express.Router()
+const Movie = require('../models/movie')
+
+router.route('/')
+    .get((req, res, next) => {
+        Movie.find((err, movies) => {
+            if (err) {
+                res.status(500)
+                return next(err)
+            }
+            return res.status(200).send(movies)
+        })
+    })
+    .post((req, res, next) => {
+        const newMovie = new Movie(req.body)
+        newMovie.save((err, savedMovie) => {
+            if (err) {
+                res.status(500)
+                return next(err)
+            }
+            return res.status(201).send(savedMovie)
+        })
+    })
+
+router.route('/:movieId')
+    .delete((req, res, next) => {
+        Movie.findOneAndDelete({ _id: req.params.movieId }, (err, deletedItem) => {
+            if (err) {
+                res.status(500)
+                return next(err)
+            }
+            return res.status(200).send(`Successfully deleted item ${deletedItem.title} from the database`)
+        })
+    })
+    .put((req, res, next) => {
+        Movie.findOneAndUpdate(
+            { _id: req.params.movieId }, // find this one to update
+            req.body, // update the object with this data
+            { new: true }, // send back the updated version please
+            (err, updatedMovie) => {
+            if (err) {
+                res.status(500)
+                return next(err)
+            }
+            return res.status(201).send(updatedMovie)
+        })
+    })
+
+module.exports = router
---
+++ week2/practice/server.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
+const mongoose = require('mongoose')
+
+// Middleware (for every request)
+app.use('/', express.json())
+app.use(morgan('dev'))
+
+// Connect to DB
+
+mongoose.connect('mongodb://localhost:27017/moviedb',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    }
+    //() => console.log('Connected to the DB')
+)
+    .then(() => console.log("Connected to MongoDB"))
+
+// Routes
+app.use('/movies', require('./routes/router'))
+
+// Error handler
+app.use((err, req, res, next) => {
+    console.log(err)
+    return res.send({ errMsg: err.message })
+})
+
+// Server Listen
+app.listen(9000, () => {
+    console.log('The server is listening on port 9000!');
+})
