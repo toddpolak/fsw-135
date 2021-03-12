8cee2e0355b7d58f87e0a7d4e576c298d2d3951e
toddpolak <67641770+toddpolak@users.noreply.github.com>
2021-03-11 19:28:53 -0800

    Practice

--- practice/client/src/context/UserProvider.js
+++ practice/client/src/context/UserProvider.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import axios from 'axios'
 
 export const UserContext = React.createContext()
 
@@ -11,10 +12,24 @@ export default function UserProvider(props) {
     }
     const [userState, setUserState] = useState(initState)
 
+    function signup(credentials) {
+        axios.post('/auth/signup', credentials)
+            .then(res => console.log(res))
+            .catch(err => console.log(err.response.data.errMsg))
+    }
+
+    function login(credentials) {
+        axios.post('/auth/login', credentials)
+        .then(res => console.log(res))
+        .catch(err => console.log(err.response.data.errMsg))
+    }
+
     return (
         <UserContext.Provider
             value={{
-                ...userState
+                ...userState, 
+                signup,
+                login
             }}>
             { props.children }
         </UserContext.Provider>
